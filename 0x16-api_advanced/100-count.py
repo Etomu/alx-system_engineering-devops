0bad5088a75f1611f492b830d654e531b23ed4f7
Etomu <etomutobi48@gmail.com>
2023-10-11 04:13:40 +0000

    new commit

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -31,8 +31,7 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
     except Exception:
         print("")
         return
-
-    results = results.get("data")
+results = results.get("data")
     after = results.get("after")
     count += results.get("dist")
     for c in results.get("children"):
@@ -52,4 +51,4 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
         instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
         [print("{}: {}".format(k, v)) for k, v in instances]
     else:
-        count_words(subreddit, word_list, instances, after, count)
+        count_words(subreddit, word_list, instances, after, count)
